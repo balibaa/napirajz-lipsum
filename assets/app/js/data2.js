6c57bb66e0f5380ffdf31779e6066770a0079e6c
balibaa <widfin@gmail.com>
2020-09-08 15:35:23 +0200

    Update and rename data.js to data2.js

---
+++ assets/app/js/data2.js
@@ -0,0 +1,6 @@
+var filenamesWords = {
+    "001_ige-T1": "latjatok vogymuk",
+    "002_FNalap": "feleim szümtükkel mik vogymuk isa pur es homu vogymuk",
+    "003_csipcsip": "csip-csi csóka vak varjúcska komámasszony kéreti a szekerét nem adhatom oda tyúkok ülnek rajta",
+    "004_rego": "hej regö rejtem azt is elfelejtem",
+};
